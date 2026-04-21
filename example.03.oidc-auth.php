04fddaf0d66328867308e4eb9803381cea0c8606
Ben Peachey <ben@muze.nl>
2026-04-21 13:41:09 +0200

    Add Client creation and OIDC discovery code to OIDC Auth example.

--- example.03.oidc-auth.php
+++ example.03.oidc-auth.php
@@ -1,11 +1,17 @@
 <?php
 
 /**
- * Example of how to
+ * Example of how to authenticate a user with OpenID Connect.
+ *
+ * Given an OIDC issuer URL, this example demonstrates how to fetch Tokens
+ * (Code, Refresh Access) needed to access a private resource.
  */
 
 namespace Potherca\Examples\Solid;
 
+use Facile\OpenIDClient\Issuer\IssuerBuilder;
+use Facile\OpenIDClient\Issuer\Metadata\Provider\MetadataProviderBuilder;
+use GuzzleHttp\Client;
 use Laminas\Diactoros\Response;
 use Laminas\Diactoros\ServerRequestFactory;
 
@@ -33,9 +39,59 @@ if (class_exists(\Whoops\Run::class) && ! isset($whoops)) {
 
 
 // =============================================================================
+// Create Client
+// -----------------------------------------------------------------------------
+$httpClientConfig = [
+    // Allow self-signed certificates for local development
+    // 'verify' => false,
+    // 'verify_host' => false,
+    // 'verify_peer' => false,
+];
+$httpClient = new Client($httpClientConfig);
+
+/* Basic Usage */
+$issuerBuilder = new IssuerBuilder();
+$metadataProviderBuilder = new MetadataProviderBuilder();
+$metadataProviderBuilder->setHttpClient($httpClient);
+// =============================================================================
+
+
+// =============================================================================
 // Handle requests
 // -----------------------------------------------------------------------------
-$content = '<h1>Example</h1>';
+// Get issuer from HTTP param
+$issuerUrl = $request->getParsedBody()['issuer']
+    ?? $request->getQueryParams()['issuer']
+    ?? '';
+
+$isRedirect = isset($request->getQueryParams()['code'])
+    || isset($request->getQueryParams()['error']);
+
+if ($issuerUrl !== '' && filter_var($issuerUrl, FILTER_VALIDATE_URL)) {
+    $issuerUrl = rtrim($issuerUrl, '/');
+    $openidDiscoveryUrl = $issuerUrl . '/.well-known/openid-configuration';
+
+    $issuer = $issuerBuilder
+        ->setMetadataProviderBuilder($metadataProviderBuilder)
+        ->build($openidDiscoveryUrl); // @throws \Http\Discovery\Exception | \Facile\OpenIDClient\Exception\ExceptionInterface
+
+    $config = $issuer->getMetadata()->toArray();
+}
+
+$outputState = empty($issuerUrl) || empty($config) ? '' : 'hidden';
+
+if ($isRedirect) {
+}
+
+$fileHandle = fopen(__FILE__, 'rb');
+fseek($fileHandle, __COMPILER_HALT_OFFSET__);
+$homepage = stream_get_contents($fileHandle);
+$content = vsprintf($homepage, [
+    '%1$s Show Form' => ! isset($exception) && $outputState ? 'hidden' : '',
+    '%2$s Show Output' => $outputState ? '' : 'hidden',
+    '%3$s Issuer URL' => $issuerUrl,
+    '%4$s OIDC Config' => isset($config) ? var_export($config, true) : '',
+]);
 $response->getBody()->write($content);
 // =============================================================================
 
@@ -62,3 +118,69 @@ echo trim($response->getBody());
 exit;
 // =============================================================================
 
+__halt_compiler();<!doctype html>
+<html color-mode="user" lang="en">
+<meta charset="utf-8">
+
+<link
+    href="data:image/svg+xml,<svg xmlns='http://www.w3.org/2000/svg' stroke='%%23000' stroke-width='0' viewBox='-5 -5 110 110'><circle cx='50' cy='50' r='51' fill='%%237C4DFF'/><circle cx='50' cy='50' r='34' fill='%%23F2E205'/><path fill='%%23FFF' stroke-width='2' d='M-1 50h16a38 35.5 0 0027.5 34.45V68.7A20 20 0 0150 30.2a20 20 0 017.5 38.5v15.75A38 35.5 0 0085 50h15A1 1 0 010 50z'/></svg>"
+    rel='icon' title='PDS Interop Logo' />
+
+<link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/mvp.css@1.17.3/mvp.min.css" />
+
+<style>
+    form { background-color: var(--color-bg-secondary); }
+    title { display: inline; }
+    .card {
+        background-color: var(--color-accent);
+        border-radius: 10px;
+        padding: 10px;
+    }
+</style>
+<header>
+    <h1><title>OIDC Server Tokens Example</title></h1>
+    <p>
+        This example demonstrates how to authenticate a user with OpenID Connect to get tokens.
+    </p>
+</header>
+<main>
+    <section>
+        <form enctype="application/x-www-form-urlencoded" method="GET" %1$s>
+            <label>
+                Issuer URL:
+                <input
+                    name="issuer"
+                    placeholder="https://idp.example.com/"
+                    required
+                    type="url"
+                    value="%3$s"
+                />
+            </label>
+            <button>Connect</button>
+        </form>
+    </section>
+    <section>
+        <output %2$s>
+            <ol>
+                <li>
+                    The OIDC Provider is <code>%3$s</code>
+                </li>
+                <li>
+                    <details>
+                        <summary>OIDC Provider Configuration</summary>
+                        <pre><code>%4$s</code></pre>
+                    </details>
+                </li>
+            </ol>
+        </output>
+    </section>
+</main>
+<footer></footer>
+<script>
+    document.querySelectorAll('output div[style]').forEach(element => {
+        element.querySelectorAll('[style]').forEach(el => el.removeAttribute('style'))
+        element.removeAttribute('style')
+        element.classList.add('card')
+    })
+</script>
+</html>
